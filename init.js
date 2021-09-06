ca5c423235b2db217507a170dc5884e31a3c9add
GrahamTCreighton <guitarrista.1988@hotmail.com>
2021-09-06 13:34:36 +0200

    DarkModeRefactored
    toggle dark mode refactored and updated!

--- init.js
+++ init.js
@@ -1,4 +1,10 @@
 const mode = localStorage.getItem("dark-mode");
 if (mode == "true") {
-  darkMode();
+  const element = document.body;
+  element.classList.add("dark-mode");
+  const myImage = document.getElementById("signature");
+  myImage.setAttribute("src", "images/signaturewhite.png");
 }
+console.log("initcall");
+
+//as an exercise try not to repeat myself, use functions with parameters.
--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-function darkMode() {
+function toggleDarkMode() {
+  console.log("toggledarkmodefunction");
   let element = document.body;
-  element.classList.toggle("dark-mode");
-  changeSignatureAndStorage();
-}
-
-function changeSignatureAndStorage() {
   let myImage = document.getElementById("signature");
-  let mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/signature.png") {
-    myImage.setAttribute("src", "images/signaturewhite.png");
-    localStorage.setItem("dark-mode", "true");
-  } else {
-    myImage.setAttribute("src", "images/signature.png");
+  const isDarkMode = localStorage.getItem("dark-mode");
+  if (isDarkMode == "true") {
+    console.log("switchingdarkmodetofalse");
+    element.classList.remove("dark-mode");
     localStorage.setItem("dark-mode", "false");
+    myImage.setAttribute("src", "images/signature.png");
+  } else {
+    console.log("switchingdarkmodetotrue");
+    element.classList.add("dark-mode");
+    localStorage.setItem("dark-mode", "true");
+    myImage.setAttribute("src", "images/signaturewhite.png");
   }
 }
 
-function hideAndShow() {} // working on hiding and showing function
+function hideAndShow() {}
+
+//use local storage to know what page to load.
+//when clicking on links setting the value on localstorage and reload.
