2a8c82319c1f93f84733bb1e51fe664333a8204d
GrahamTCreighton <guitarrista.1988@hotmail.com>
2021-09-10 11:21:12 +0200

    dark mode redone

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 function toggleDarkMode() {
   const element = document.body;
-  const myImage = document.getElementById("signature");
   const isDarkMode = localStorage.getItem("dark-mode");
   if (isDarkMode == "true") {
     element.classList.remove("dark-mode");
-    localStorage.setItem("dark-mode", "false");
-    myImage.setAttribute("src", "images/signature.png");
+    setItemAttribute("false", "images/signature.png");
   } else {
     element.classList.add("dark-mode");
-    localStorage.setItem("dark-mode", "true");
-    myImage.setAttribute("src", "images/signaturewhite.png");
+    setItemAttribute("true", "images/signaturewhite.png");
   }
+}
+
+function setItemAttribute(boolean, image) {
+  const myImage = document.getElementById("signature");
+  localStorage.setItem("dark-mode", boolean);
+  myImage.setAttribute("src", image);
 }
 
 function hide() {
@@ -19,14 +22,13 @@ function hide() {
     mainPage[i].style.display = "none";
   }
 }
+
 function show(elementID) {
   const showProject = document.getElementById(elementID);
   showProject.style.display = "block";
 }
+
 function hideAndShow(elementID) {
   hide();
   show(elementID);
 }
-
-//use local storage to know what page to load.
-//when clicking on links setting the value on localstorage and reload.
